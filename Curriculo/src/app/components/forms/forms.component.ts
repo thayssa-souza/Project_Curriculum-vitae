f785141de04342e4bc0732e7038da4164181555d
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-12-15 12:29:04 -0300

    Add validators on forms

--- Curriculo/src/app/components/forms/forms.component.ts
+++ Curriculo/src/app/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ContactFormData } from 'src/models/contact-form-data.models';
 
@@ -13,24 +13,29 @@ export class FormsComponent {
 
   ngOnInit(){
     this.buildForm();
-
   }
 
   private buildForm(): void{
     this.form = new FormGroup({
-      nome: new FormControl(),
-      email: new FormControl(),
-      message: new FormControl(),
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern("^[a-z]$")
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("^[a-z0-9._+-]+@[a-z0-9.-]+\\.[a-z]{2}$")
+      ]),
+      message: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
     });
-
   }
 
   public submitForm(): void {
-    //this.sendForm.emit(this.form);
-    alert("mensagem enviada")
+    alert("Mensagem enviada!")
     console.log(this.form);
    }
-
-
-
 }
